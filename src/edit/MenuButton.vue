718e6ff99a92d0d3de5253f565aa09ed49f15768
Zdravko Curic <zcuric@gmail.com>
2021-04-12 16:24:29 +0200

    Add MenuButton component

---
+++ src/edit/MenuButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-btn
+    @click="command"
+    icon
+    :class="{ 'active': isActive }"
+    class="menu-button mx-1">
+    <v-icon>{{ `mdi-${icon}` }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'tce-tiptap-menu-button',
+  props: {
+    command: { type: Function, required: true },
+    isActive: { type: Boolean, default: false },
+    icon: { type: String, required: true }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-button {
+  min-width: 34px;
+  height: 34px;
+  background: transparent;
+  border: none;
+
+  .mdi {
+    color: #333;
+    font-size: 20px;
+    line-height: 20px;
+    vertical-align: top;
+  }
+
+  &.active {
+    .mdi {
+      color: #ff6590;
+    }
+
+    background: #ffe5ec;
+  }
+}
+</style>
